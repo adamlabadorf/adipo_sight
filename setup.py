4ad74769b578ce6040c829a83a13fc4931764594
Adam Labadorf <alabadorf@gmail.com>
2012-07-05 15:31:32 -0400

    moved data directory into src tree, modified setup.py appropriately

--- setup.py
+++ setup.py
@@ -8,14 +8,17 @@ from distutils.core import setup
 scripts = ['scripts/cherry_sight.py',
           ]
 
+data_dirs = ['data/tmpl',
+            ]
+
 # setup and install
 setup(name='adipo_sight',
       version='0.5',
       author='Adam Labadorf',
       author_email='alabadorf@gmail.com',
-      package_dir={'':'src'},
-      py_modules=['chipsequtil.db','chipsequtil.mww','chipsequtil.log',]
+      package_dir={'adipo_sight':'src'},
+      py_modules=['adipo_sight.db','adipo_sight.mww','adipo_sight.log',]
       packages=['adipo_sight'],
-      package_data={'': ['org_settings.cfg']},
+      package_data={'adipo_sight': data_dirs},
       scripts=scripts,
      )
